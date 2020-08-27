0e0dd214195d80f13529aa924fb3fa24f1fce549
Preston Davis <prestonseandavis101@yahoo.com>
2020-08-27 02:08:24 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,23 @@
-# Write your code here.
+
+
+def line(katz_deli)
+  if katz_deli.length == 0
+    puts "The line is currently empty."
+  else
+    new_array = []
+   katz_deli.each_with_index do |name, num|
+      line_num = num + 1 
+      new_array << "#{line_num}. #{name}"
+    end
+      puts "The line is currently: " + new_array.join(" ")
+   end
+end
+
+def take_a_number(array, name)
+  array.push(name)
+  num = array.index(name) + 1
+  puts "Welcome, #{name}. You are number #{num} in line."
+  
+  end
+  
+  def now_serving
